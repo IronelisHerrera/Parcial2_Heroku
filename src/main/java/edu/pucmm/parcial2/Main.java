b4ec0b131be2df5acf4f6cb06870fdda66bd6c12
Ironelis Herrera <ironelisherrerav@gmail.com>
2019-12-07 19:40:29 -0500

    Preparando para subir a heroku, man

---
+++ src/main/java/edu/pucmm/parcial2/Main.java
@@ -0,0 +1,12 @@
+package edu.pucmm.parcial2;
+
+import edu.pucmm.parcial2.Handler.*;
+import edu.pucmm.parcial2.Services.StartDatabase;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StartDatabase.getInstancia().startDb();
+        new mainHandler().startup();
+
+    }
+}
